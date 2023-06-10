9678ca2dc2b364afd1a9676c06240ec2a2974dfa
Alejandro Mira-Agudelo <47834036+alema08@users.noreply.github.com>
2023-06-10 17:13:34 -0500

    corrección en la configuración de seguridad

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.include_router(monitoreo)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credential=True,
     allow_methods=["*"],
     allow_headers=["*"],
